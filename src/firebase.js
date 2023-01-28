412c7a3dc17ce1cdbcd866448e09a6553ec26991
pawitpr <123424956+pawitpr@users.noreply.github.com>
2023-01-28 18:13:04 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,24 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 import 'firebase/storage';
 
-const firebaseConfig = {};
+const firebaseConfig = {
+
+ apiKey: "AIzaSyAPMTs5tROpZ2L8Zsz1JSleUzAa4AOOFFQ",
+
+  authDomain: "backend-web-38f25.firebaseapp.com",
+
+  projectId: "backend-web-38f25",
+
+  storageBucket: "backend-web-38f25.appspot.com",
+
+  messagingSenderId: "937809457219",
+
+  appId: "1:937809457219:web:ad3fb3594e9f67fdc9a4f6",
+
+  measurementId: "G-G7LKVNKMCC"
+
+
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
